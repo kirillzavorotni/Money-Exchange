d48c1a477b38cff1052412bdd6251ad5098ef2f9
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2018-09-16 17:26:09 +0300

    created method makeExchange to determines the min number of coins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    if(currency <= 0) return {};
+    if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    var result = {};
+    var coins = {
+        'H': 50,
+        'Q': 25,
+        'D': 10,
+        'N': 5,
+        'P': 1
+    };
+
+    for(var key in coins){
+
+        var cur = currency / coins[key]; // 0, 1,72, 1,8, 1,6, 3
+        if(Math.floor(cur) === 0){
+            continue;
+        } else {
+            var quantity = Math.floor(cur);
+            result[key] = quantity;
+            currency -= coins[key] * quantity;
+            if(currency === 0){
+                return result;
+            }
+        }
+    }
+
+    return result;
+
 }
